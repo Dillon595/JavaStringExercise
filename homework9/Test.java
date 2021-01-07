ca355472b77d5143056de3676604f71e3edef476
Alun <39848824+Dillon595@users.noreply.github.com>
2021-01-07 14:50:16 +0800

    Update Test.java

--- homework9/Test.java
+++ homework9/Test.java
@@ -6,11 +6,11 @@ public class Test {
 	public static void main(String[] args) {
 		Person p1 = new Person("张三", 20, 55);
 		Person p2 = new Person("张三", 20, 55);
-		System.out.println(p1.equals(p2));
+		System.out.println(p1.equals(p2));		//此处调用的为在Person中重写的equals方法
 
 		Person p3 = new Person("张三", 30, 55);
 		Person p4 = new Person("李四", 40, 56);
-		System.out.println(p3.Max(p4));
+		System.out.println(p3.Max(p4));			//此处Max（）为Person中的方法
 	}
 
 }
