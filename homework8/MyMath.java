f4a36c4f7790c58c3b48a5f0ea01153b02345e68
Alun <39848824+Dillon595@users.noreply.github.com>
2021-01-07 14:45:31 +0800

    Update MyMath.java

--- homework8/MyMath.java
+++ homework8/MyMath.java
@@ -3,15 +3,15 @@ package homework8;
 
 /**  
  * @ClassName: MyMath
- * @Description: 
+ * @Description: 一个四舍五入类实现：能对double型数据的指定小数位取整，3.1456精确到小数点后2位：3.1456==>314.56==>315==>3.15
  * @author LYL
  * @date 2021-01-07 13:55:58
 */
 
 public class MyMath {
 	public static double haha(double d) {
-		double test = Math.round(d*100);
-		return test/100;
+		double test = Math.round(d*100);	//因为需精确到小数点后两位，所以先将输入的数字*100后四舍五入
+		return test/100;			//再将*100后的数字精度恢复成小数点后两位
 		
 	}
 }
