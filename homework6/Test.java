061c11dd7f89110d954963b718989fd3acae5c72
Alun <39848824+Dillon595@users.noreply.github.com>
2021-01-07 14:40:50 +0800

    Update Test.java

--- homework6/Test.java
+++ homework6/Test.java
@@ -1,6 +1,6 @@
 /**  
  * @Title: Test.java
- * @Description: 
+ * @Description: 在控制台任意输入一个字符串，统计其中大写字母的个数，小写字母的个数，数字的个数，其他符号的个数
  * @author LYL
  * @date 2021-01-07 13:20:37
  */
@@ -18,20 +18,20 @@ import java.util.Scanner;
 
 public class Test {
 	public static void check(String str) {
-		int upNumber = 0;
-		int lowNumber = 0;
-		int number = 0;
-		int otherNumber = 0;
+		int upNumber = 0;	//大写字母数
+		int lowNumber = 0;	//小写字母数
+		int number = 0;		//数字数
+		int otherNumber = 0;	//其它字符数
 		
-		char []	arr = str.toCharArray();
-		for(int i=0;i<arr.length;i++) {
-			if(Character.isUpperCase(arr[i])) {
+		char []	arr = str.toCharArray();			//先将输入的字符串转换为char型数组
+		for(int i=0;i<arr.length;i++) {				//循环遍历char型数组
+			if(Character.isUpperCase(arr[i])) {		//如果是大写字母
 				upNumber++;
-			}else if(Character.isLowerCase(arr[i])) {
+			}else if(Character.isLowerCase(arr[i])) {	//如果是小写字母
 				lowNumber++;
-			}else if(Character.isDigit(arr[i])){
+			}else if(Character.isDigit(arr[i])){		//如果是数字
 				number++;
-			}else {
+			}else {						//其它
 				otherNumber++;
 			}
 		}
